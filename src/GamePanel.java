270eb16db2527680cb92bdb13f2fad26c1e3a7d8
Muhammad Hamza <muhammad.hamza6415@gmail.com>
2023-01-13 18:57:54 -0500

    set vars in game panel

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,10 +1,75 @@
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Random;
+
+import javax.swing.JPanel;
+
 
 public class GamePanel extends JPanel implements ActionListener{
+
+    static final int SCREEN_WIDTH=600;
+    static final int SCREEN_HEIGHT=600;
+    static final int UNIT_SIZE = 25;
+    static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;
+    static final int DELAY = 75;
+
+    final int x[] = new int[GAME_UNITS];
+    final int y[] = new int[GAME_UNITS];
+    int bodyParts = 6;
+    int appleEaten;
+    int appleX;
+    int appleY;
+    char direction = 'R';
+    boolean running = false;
+    Timer timer;
+    Random random;
+
+
+
+    GamePanel(){
+
+    }
+
+    public void startGame(){
+
+    }
+
+    public void paintComponent(Graphics g){
+
+    }
+
+    public void draw(Graphics g){
+
+    }
+
+    public void move(){
+
+    }
+    public void checkApple(){
+
+    }
+
+    public void checkCollisions(){
+
+    }
+
+    public void gameOver(Graphics g){
+
+    }
+
+
     @Override
     public void actionPerformed(ActionEvent e){
         //gonna add smt here
     }
+
+    public class MyKeyAdapter extends KeyAdapter{
+        @override
+        public void keyPresser(KeyEvent e){
+
+        }
+    }
+
+
 }
--- src/snakeGame.java
+++ src/snakeGame.java
@@ -1,6 +1,8 @@
 public class snakeGame {
 
     public static void main(String[] args){
+        System.out.println("from snake game file");
 
+        new GameFrame();
     }
 }
